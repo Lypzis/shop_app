2cee993e133cf066a370aa1e52a2449f0f30a1b4
Lypzis <victorvp2008@hotmail.com>
2020-03-29 17:22:21 -0300

    Update

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,37 @@
-export const ADD_ORDER = "ADD_ORDER";
+import ApiEndPoint from '../../constants/ApiEndPoint';
+
+export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = order => {
-	return {
-		type: ADD_ORDER,
-		order: order
+	return async dispatch => {
+		const { cartItems, date, total } = order;
+
+		try {
+			const res = await fetch(`${ApiEndPoint.api}/orders.json`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					cartItems: cartItems,
+					date: date,
+					total: total
+				})
+			});
+
+			if (!res.ok) throw new Error('Sorry, could not sent the order.');
+
+			const resData = await res.json();
+
+			dispatch({
+				type: ADD_ORDER,
+				order: {
+					id: resData.name,
+					cartItems: cartItems,
+					date: date,
+					total: total
+				}
+			});
+		} catch (err) {
+			throw err;
+		}
 	};
 };
